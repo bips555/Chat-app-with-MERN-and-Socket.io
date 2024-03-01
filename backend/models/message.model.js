3975bf8f6ea34b0decab55bea17478c40f4df1ee
biplove subedi <subedi.biplove10@gmail.com>
2024-03-01 18:03:42 +0545

    message model created with senderId, receiverId and message

---
+++ backend/models/message.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+const messageSchema = mongoose.Schema({
+senderId:
+{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:true
+},
+receiverId:
+{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:true
+},
+message:
+{
+    type:String,
+    required:true
+}},
+{
+    timestamps:true
+
+}
+)
+const Message = mongoose.Model("Message",messageSchema)
+
+export default Message
