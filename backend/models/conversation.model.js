cd48ce15ab6049f2fff77dc3ed7a7e656e8797e9
biplove subedi <subedi.biplove10@gmail.com>
2024-03-01 18:07:59 +0545

    Conversation model created with participants containing user array and messages containing message array

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,28 @@
+import mongoose, { mongo } from "mongoose";
+const conversationSchema = mongoose.Schema({
+participants:
+[
+    {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+]
+,
+messages:
+[
+    {
+type:mongoose.Schema.Types.ObjectId,
+ref:"Message",
+default:[]
+
+    }
+]
+},
+{
+    timestamps:true
+
+}
+)
+const Conversation = mongoose.Model("Conversation",conversationSchema)
+
+export default Conversation
