a7d01d4c55b61520f81a23eed9656020bd441440
biplove subedi <subedi.biplove10@gmail.com>
2024-03-02 12:25:09 +0545

    creating login form using css and daisyui

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,19 @@
+const Login = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
+        <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+          <h1 className="text-3xl font-semibold text-center text-gray-300">
+            Login
+            <span className="text-blue-500 px-3">
+                ChatApp
+            </span>
+          </h1>
+          Login
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
